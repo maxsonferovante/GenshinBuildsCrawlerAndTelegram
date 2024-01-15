38c64a8d1407695d05733665a2ce56f2813ffa67
Maxson Almeida <maxsonferovante@gmail.com>
2024-01-15 02:56:38 -0300

    Update Telegram token and URL configuration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import GenshinBuildsMaalBot from './Telegram/GenshinBuildsMaalBot.js';
-
+import dotenv from 'dotenv';
 
 (async () => {
+    dotenv.config(
+        {
+            path: '.env'
+        }
+    );
     const genshinBuildsMaalBot = new GenshinBuildsMaalBot();
     await genshinBuildsMaalBot.initBot();
 })();
