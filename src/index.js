d55604a6ba00a84a5ec077da0198cd866a2c1f21
Maxson Almeida <maxson@devnology.com.br>
2024-01-15 02:27:09 -0300

    Remove dotenv configuration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import GenshinBuildsMaalBot from './Telegram/GenshinBuildsMaalBot.js';
-import dotenv from 'dotenv';
+
 
 (async () => {
-    dotenv.config(
-        {
-            path: '.env'
-        }
-    );
     const genshinBuildsMaalBot = new GenshinBuildsMaalBot();
     await genshinBuildsMaalBot.initBot();
 })();
