0226d54e715562ecdfb2b4b22aac375f9de66c66
= <=>
2024-01-18 00:30:54 -0300

    Add PostgreUserRepository class

--- src/database/prisma.js
+++ src/database/prisma.js
@@ -3,6 +3,21 @@ import {
 } from '@prisma/client'
 
 
-const prisma = new PrismaClient()
+/**
+ * @description: Cria uma instância do PrismaClient
+ * @returns {PrismaClient} 
+ * @throws {Error}
+ * @example
+ * const prisma = new PrismaClient()
+ * log: ['query', 'info', 'warn']
+ */
+let prisma
+try {
+    prisma = new PrismaClient()    
+} catch (error) {
+    console.log(error)
+    throw new Error('Error em conectar com o banco de dados')
+}
+
 
 export default prisma
