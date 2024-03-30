c3791a7c97bb98b80b461f86fdbbcd42a1bafd1f
maxsonferovante <maxsonferovante@gmail.com>
2024-03-30 20:08:15 -0300

    Removidos arquivos e código não utilizados

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import GenshinBuildsMaalBot from './telegram/genshinBuildsMaalBot.js';
-import WebCrawlerGenshinBuildsService from './webCrawler/WebCrawlerGenshinBuildsService.js';
 import prisma from './database/prisma.js'
 import dotenv from 'dotenv';
 
 (async () => {
-    dotenv.config(
-        {
-            path: '.env'
-        }
-    );
-    const webCrawlerGenshinBuildsService = new WebCrawlerGenshinBuildsService();
-    const genshinBuildsMaalBot = new GenshinBuildsMaalBot();
+    dotenv.config({path: '.env'});
 
+    const genshinBuildsMaalBot = new GenshinBuildsMaalBot();
+    
     try {
-        await webCrawlerGenshinBuildsService.run()
         await genshinBuildsMaalBot.initBot()     
     } catch (error) {
         console.log(error)
