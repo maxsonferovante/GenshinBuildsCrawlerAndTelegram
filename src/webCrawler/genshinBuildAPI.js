33f4a487fc7954f6b8ca1f7db636db27f7b35445
maxsonferovante <maxsonferovante@gmail.com>
2025-01-19 20:16:03 -0300

    refactor: Atualizar URLs da API para usar variável de ambiente e remover código comentado

--- src/webCrawler/genshinBuildAPI.js
+++ src/webCrawler/genshinBuildAPI.js
@@ -1,55 +1,38 @@
 import axios from "axios";
 export default class genshinBuildAPI {
+  constructor() {
+    this.dictWeapon = {};
+    this.dictCharacter = {};
+    this.url_base = process.env.URL_API;
+  }
 
-    constructor() {
-        this.dictWeapon = {};
-        this.dictCharacter = {};
-
+  async getWeaponList(chatId) {
+    try {
+      const response = await axios.get(`${this.url_base}/crawler/weapons`, {
+        timeout: 30 * 1000,
+      });
+      console.log(
+        `Solicitação - chatId ${chatId} -> response: ${response.status} - ${response.data["created_at"]}`
+      );
+      this.dictWeapon = response.data.data;
+    } catch (error) {
+      console.log(`Solicitação - chatId ${chatId} -> error: ${error}`);
+      throw new Error(`Solicitação - chatId ${chatId} -> error: ${error}`);
     }
+  }
 
-    async getWeaponList(chatId) {
-        /* 
-        curl -X 'GET' \
-        'http://192.168.2.121:8000/crawler/weapons' \
-        -H 'accept: application/json'
-        */
-        try {
-            const response = await axios.get('http://0.0.0.0:8000/crawler/weapons',{timeout: 30 * 1000,});
-            console.log(`Solicitação - chatId ${chatId} -> response: ${response.status} - ${response.data['created_at']}`);
-            this.dictWeapon = response.data.data;
-        } catch (error) {
-            console.log(`Solicitação - chatId ${chatId} -> error: ${error}`);
-            throw new Error(`Solicitação - chatId ${chatId} -> error: ${error}`);
-        }
+  async getCharacterList(chatId) {
+    try {
+      const response = await axios.get(`${this.url_base}/crawler/characters`, {
+        timeout: 30 * 1000,
+      });
+      console.log(
+        `Solicitação - chatId ${chatId} -> response: ${response.status} - ${response.data["created_at"]}`
+      );
+      this.dictCharacter = response.data.data;
+    } catch (error) {
+      console.log(`Solicitação - chatId ${chatId} -> error: ${error}`);
+      throw new Error(`Solicitação - chatId ${chatId} -> error: ${error}`);
     }
-
-    async getCharacterList(chatId) {
-        /* curl -X 'GET' \
-        'http://192.168.2.121:8000/crawler/characters' \
-        -H 'accept: application/json' */
-        try {
-            const response = await axios.get('http://0.0.0.0:8000/crawler/characters',{timeout: 30 * 1000,});
-            console.log(`Solicitação - chatId ${chatId} -> response: ${response.status} - ${response.data['created_at']}`);
-            this.dictCharacter = response.data.data;
-        } catch (error) {
-            console.log(`Solicitação - chatId ${chatId} -> error: ${error}`);
-            throw new Error(`Solicitação - chatId ${chatId} -> error: ${error}`);
-        }
-    }
-    
-    
-
+  }
 }
-
-
-const crawler = new genshinBuildAPI();
-
-// crawler.getWeaponList().then((response) => {
-//     console.log(response);
-// }).catch((error) => {
-//     console.log(error);
-// });
-
-// crawler.getCharacterList().then((response) => {
-//     console.log(response);
-// }).catch((error) => {});
